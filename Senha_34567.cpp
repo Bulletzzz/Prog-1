697db4d53c81edf82327fff9bf8f2d5c4dcaec01
Bulletzzz <160980594+Bulletzzz@users.noreply.github.com>
2024-04-07 19:06:53 -0300

    Add files via upload

---
+++ Senha_34567.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char Senha;
+ cout<<"Qual o codigo?";
+ cin>>Senha;
+
+if (Senha=34567){
+    cout<<"Acesso autorizado";
+}
+else{
+    cout<<"Acesso negado";
+}
+
+return 0;
+}
