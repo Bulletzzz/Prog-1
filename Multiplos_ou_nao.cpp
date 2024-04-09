6a72de8442c7dedd3ccdde61ec65ba8ff2731b43
Bulletzzz <160980594+Bulletzzz@users.noreply.github.com>
2024-04-09 02:17:11 -0300

    Add files via upload

---
+++ Multiplos_ou_nao.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+
+    cout <<"Por favor digite dois numeros";
+    cin >> n1>> n2;
+
+    if (n1 % n2 == 0 || n1 % n2 == 0) {
+        cout << "Sao multiplos" << endl;
+    } else {
+        cout << "Nao sao multiplos" << endl;
+    }
+
+    return 0;
+}
